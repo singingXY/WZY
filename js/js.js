87fb11c35397890beb2fe90e70abfad2d485b6df
Hsing <346175922@qq.com>
2019-07-10 16:01:48 +0800

    con

--- js/js.js
+++ js/js.js
@@ -2,8 +2,6 @@
 function closeTop(){
     document.getElementById('top_msg').style.display='none';
     setCookie("topmsg","yes");
-    console.log("2"+ document.cookie );
-    console.log(document.cookie.indexOf("topmsg="));
 }
 
 function setCookie(cname,cvalue){
@@ -11,7 +9,6 @@ function setCookie(cname,cvalue){
 }
 
 function onloadcookie(){
-    console.log(document.cookie.indexOf("topmsg="));
     if(document.cookie.indexOf("topmsg=")==-1) {
         document.getElementById('top_msg').style.display='block';
     }
