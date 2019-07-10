6ef6893556d17a8d127f2f20cd48d88c1ba09a45
Hsing <346175922@qq.com>
2019-07-10 17:36:25 +0800

    开始做ajax

--- js/js.js
+++ js/js.js
@@ -3,7 +3,7 @@ function closeTop(){
     document.getElementById('top_msg').style.display='none';
     setCookie("topmsg","yes");
 }
-
+//
 function setCookie(cname,cvalue){
     document.cookie = cname + "=" + cvalue + ";path=/";
 }
@@ -15,3 +15,30 @@ function onloadcookie(){
 }
 
 onloadcookie();
+
+
+//AJAX
+function loadXMLDoc(url,cfunc){
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.onreadystatechange=cfunc;
+    xmlhttp.open("GET",url,true);
+    xmlhttp.send();
+}
+
+function couresByCategory()
+{
+    var pageNo = document.getElementsByClassName('pagesindex').innerHTML;
+    var type = "10";
+    if(document.getElementById('tab-nav'))
+    loadXMLDoc("http://study.163.com/webDev/couresByCategory.htm?pageNo="+ pageNo +"&psize=20&type="+ type,function(){
+        if (xmlhttp.readyState==4 && xmlhttp.status==200){
+            document.getElementById('tab-con').getElementsByTagName('ul').innerHTML = xmlhttp.responseText;
+            
+        }else{
+            alert("发生错误"+xmlhttp.status);
+        }
+    })
+
+
+
+}
